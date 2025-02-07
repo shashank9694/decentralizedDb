68455ea46e050e19ce4f517fb78855ccd48b20f6
shashank9694 <shashanksingh9694@gmail.com>
2025-02-07 15:40:56 +0530

    localhost - 0.0.0.0

--- backend/config/mongo.config.ts
+++ backend/config/mongo.config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://0.0.0.0:27017/mydatabase";
 
 const connectDB = async () => {
   try {
--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,6 @@ import app from './app'
 
 const port = 4000
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0' () => {
   console.log(`App listening at http://localhost:${port}`)
 })
