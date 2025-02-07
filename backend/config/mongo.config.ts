97327273ce70444bb68d1a21899b08fed32fa758
shashank singh <68474268+shashank9694@users.noreply.github.com>
2025-02-07 14:45:24 +0530

    Update mongo.config.ts

--- backend/config/mongo.config.ts
+++ backend/config/mongo.config.ts
@@ -3,27 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_URI = "mongodb+srv://shashanksingh9694:Sha789@#$@cluster0.l7jmt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase";
 
-const connectDB = async (retries = 5, delay = 5000) => {
+const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+     
     } as mongoose.ConnectOptions);
 
     console.log("✅ MongoDB Connected Successfully");
+    return
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error);
-
-    if (retries > 0) {
-      console.log(`🔄 Retrying in ${delay / 1000} seconds... (${retries} attempts left)`);
-      setTimeout(() => connectDB(retries - 1, delay), delay);
-    } else {
-      console.error("❌ MongoDB Connection Failed after multiple retries.");
-      process.exit(1);
-    }
+    process.exit(1);
   }
 };
 
 export default connectDB;
+
+
